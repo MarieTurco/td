d1b5f8c386a30b848dc7ad10e6a79eea57edc14e
MarieTurco <marieturco.07@gmail.com>
2021-01-19 08:30:50 +0000

    Addition pour les points en 1D

---
+++ main.py
@@ -0,0 +1,30 @@
+from collections import namedtuple
+
+#Point à 2D
+Point2D=namedtuple('Point2D', ['x','y'])
+
+p2d = Point2D(x=0,y=1)
+print(p2d)
+
+print(p2d[0])
+
+
+#Point à 1D
+Point1D = namedtuple("Point1D",["x"])
+print(Point1D)
+
+p_04=Point1D(0.4)
+print(p_04)
+p_05=Point1D(0.5)
+print(p_04+p_05)
+
+#Ce qu'on voudrait 
+#p_04+p_05 == Point1D(0.9)
+#(0.4,0.5)
+
+def ajout_point1d(point1, point2):
+  x = point1[0]+point2[0]
+  print(x)
+  return Point1D(x)
+
+print(f"Addition ok :{ajout_point1d(p_04,p_05)} ")
