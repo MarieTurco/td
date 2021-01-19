ce718d5a829a4f3501b2673634ee5294460b7470
MarieTurco <marieturco.07@gmail.com>
2021-01-19 08:55:10 +0000

    Différentes manières d'ajouter

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ p_09=ajout_point1d(p_04,p_05)
 
 print (norme1d(p_09))
 
+#Retout au Point2D
 Point2D=namedtuple('Point2D',' x y ')
 
 p1=Point2D(0,1)
@@ -43,3 +44,28 @@ p2=Point2D(1,0)
 
 print(p1)
 print(p2)
+
+def ajout_point2d(p_1,p_2):
+  x=p_1[0]+p_2[0]
+  y=p_1[1]+p_2[1]
+  return Point2D(x,y)
+
+p3 = ajout_point2d(p1,p2)
+
+print(p3)
+
+print(f"Addition ok : {ajout_point2d(p1,p2) == Point2D(1,1)}")
+
+def ajout_point2d(p_1,p_2):
+  coord=[]
+  for indice in range((len(p_1))) :
+    coord.append(p_1[indice]+p_2[indice])
+  return Point2D(coord[0],coord[1])
+
+print(f"Addition ok : {ajout_point2d(p1,p2) == Point2D(1,1)}")
+
+def ajout_point2d(p_1,p_2):
+  coord=tuple(x+y for x,y in zip(p_1,p_2))
+  return Point2D(coord[0],coord[1])
+
+print(f"Addition ok : {ajout_point2d(p1,p2) == Point2D(1,1)}")
